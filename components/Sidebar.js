01b1d22f694082499524ac329dedcd4f676cf131
vineeta <vineetadevnani@gmail.com>
2026-06-05 16:30:36 +0530

    fix logo blend

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -62,7 +62,7 @@ export default function Sidebar() {
       {/* Logo */}
       <div style={{ padding: '20px 24px', display: 'flex', alignItems: 'center', background: '#1E293B' }}>
         <img
-          src="/kreativlogo.png"
+          src="/kreativlogo1.png"
           alt="Kreativ Lift"
           style={{ width: '200px', height: 'auto', objectFit: 'contain', borderRadius: '8px' }}
         />
