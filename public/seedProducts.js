35fc9b3812645cea7e27f54b0c3f633477d9fe00
vineeta <vineetadevnani@gmail.com>
2026-06-05 15:28:45 +0530

    fix: mongodb connection

--- public/seedProducts.js
+++ public/seedProducts.js
@@ -3,7 +3,7 @@
 
 const mongoose = require('mongoose');
 
-const MONGODB_URI = 'mongodb+srv://vineetadevnani_db_user:vdevnani007@cluster0.ksvbtfj.mongodb.net/stocksystem';
+const MONGODB_URI = 'mongodb+srv://tasha_db_user:kreativlift1234@cluster0.nph5g5d.mongodb.net/stockvault?appName=Cluster0';
 // Example: 'mongodb+srv://vineetadevnani_db_user:vdevnani007@cluster0.ksvbtfj.mongodb.net/stocksystem'
 
 const productSchema = new mongoose.Schema({
