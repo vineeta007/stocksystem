6b55ec15f3bd59184f5c45efc56122f88eae11b6
vineeta <vineetadevnani@gmail.com>
2026-06-29 16:21:28 +0530

    changes in expemses

--- models/Expense.js
+++ models/Expense.js
@@ -5,7 +5,7 @@ const ExpenseSchema = new mongoose.Schema({
   name:     { type: String, required: true, trim: true },
   amount:   { type: Number, required: true, default: 0 },
   details:  { type: String, default: '' },
-  category: { type: String, default: 'bank_payment' },
+  category: { type: String, default: 'biaya_operasional' },
 }, { timestamps: true });
 
 export default mongoose.models.Expense || mongoose.model('Expense', ExpenseSchema);
